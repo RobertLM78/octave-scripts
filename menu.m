9f830ea02e1e0ab71f46d6912618294d10e87fb1
RobertLM78 <beannachtai@hotmail.com>
2022-03-15 17:36:35 -0600

    Add files via upload

--- menu.m
+++ menu.m
@@ -1,12 +1,35 @@
-function Slctn = menu(MenuItems)
+## Copyright (C) 2017 Robert B. Lock
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {Function File} {@var{selection} =} menu ([@var{menuitems}])
 # Title: menu.m ; function Slctn = menu(MenuItems)
-# Version: 0.1 - April 2017
-# Author: Robert Lock - beannachtai@homtail.com
+# Version: 0.2 - April 2017
+# Author: Robert Lock - beannachtai-at-homtail.com
 # License: GPL v3
-# Usage:  menu(["Title";"Option1";"Option2";....;"OptionN";"Quit"])
+# Usage:  selection = menu(["Title";"Option1";"Option2";....;"OptionN";"Quit"])
 #         inputs a menu item list and outputs a selection from menu
-# About:  A generic menu function for console menu-driven scripts. 
+# About:  A generic menu function for menu-driven scripts.  A primary goal of
+#         this function will be to replace the default menu.m function that
+#         comes bundled with GNU Octave.
 # -----------------------------------------------------------------------------
+function Slctn = menu(MenuItems)
+
 if (nargin ~= 1)
 	print_usage ();
 endif
